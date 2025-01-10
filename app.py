a063805966164f558b4aeb06eb1308f765ca14d9
Abhishek Kori <abhishekkori601@gmail.com>
2025-01-10 20:50:16 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pickle 
+import re
+
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+port_stem = PorterStemmer()
+
+
+
+def stemming(text):
+    stammed_contant = re.sub('[^a-zA-Z]', ' ', text)
+    stammed_contant = stammed_contant.lower()
+    stammed_contant = stammed_contant.split()
+    stammed_contant = [port_stem.stem(word) for word in stammed_contant if word not in stopwords.words('english')]
+    stammed_contant = ' '.join(stammed_contant) 
+
+    return stammed_contant
+
+
+cv = pickle.load(open("vectorizer.pkl","rb"))
+model = pickle.load(open("model.pkl", "rb"))
+
+
+st.title("SMS Spam Classifier")
+
+input_sms = st.text_input("Enter Message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = stemming(input_sms) # type: ignore
+    # 2. vectorize
+    vector_input = cv.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
+
+
